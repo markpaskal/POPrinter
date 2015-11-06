00cacfea5daa4f90c8f99a8313943ba50e07eeb0
Mark Paskal <markpaskal@gmail.com>
2015-11-05 21:48:07 -0700

    Added license files.

--- POPrinter/My Project/Resources.Designer.vb
+++ POPrinter/My Project/Resources.Designer.vb
@@ -61,6 +61,47 @@ Namespace My.Resources
         End Property
         
         '''<summary>
+        '''  Looks up a localized string similar to                     GNU GENERAL PUBLIC LICENSE
+        '''                       Version 3, 29 June 2007
+        '''
+        ''' Copyright (C) 2007 Free Software Foundation, Inc. &lt;http://fsf.org/&gt;
+        ''' Everyone is permitted to copy and distribute verbatim copies
+        ''' of this license document, but changing it is not allowed.
+        '''
+        '''                            Preamble
+        '''
+        '''  The GNU General Public License is a free, copyleft license for
+        '''software and other kinds of works.
+        '''
+        '''  The licenses for most software and other practical works are designed
+        '''to [rest of string was truncated]&quot;;.
+        '''</summary>
+        Friend ReadOnly Property COPYING() As String
+            Get
+                Return ResourceManager.GetString("COPYING", resourceCulture)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized string similar to     Copyright 2015 Mark Paskal
+        '''
+        '''    This program is free software: you can redistribute it and/or modify
+        '''    it under the terms of the GNU General Public License as published by
+        '''    the Free Software Foundation, either version 3 of the License, or
+        '''    (at your option) any later version.
+        '''
+        '''    This program is distributed in the hope that it will be useful,
+        '''    but WITHOUT ANY WARRANTY; without even the implied warranty of
+        '''    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        '''    GNU General Public [rest of string was truncated]&quot;;.
+        '''</summary>
+        Friend ReadOnly Property License() As String
+            Get
+                Return ResourceManager.GetString("License", resourceCulture)
+            End Get
+        End Property
+        
+        '''<summary>
         '''  Looks up a localized string similar to Error, the path you supplied is no longer valid!.
         '''</summary>
         Friend ReadOnly Property OnLoadErrorText() As String
