c60d206f3c8e425b83d74872e75b0d0f7be18875
Mark Paskal <markpaskal@gmail.com>
2015-11-08 20:00:21 -0700

    Now enumerates supported formats in an array and adds watchers for them programaticly.

--- POPrinter/Form1.vb
+++ POPrinter/Form1.vb
@@ -3,7 +3,21 @@ Imports System.IO
 
 Public Class Form1
 
-    Private Sub NewFile(sender As Object, e As FileSystemEventArgs) Handles XPSWatcher.Created, PDFWatcher.Created, JPGWatcher.Created
+    Dim SupportedFormats As String() = {"xps", "pdf", "jpg", "jpeg", "svg", "png", "webp", "gif", "htm", "html"}
+    Dim Watchers As New List(Of FileSystemWatcher)
+    Sub MakeWatchers()
+        For Each format As String In SupportedFormats
+            Dim fsw As New FileSystemWatcher
+            fsw.Filter = "*." & format
+            fsw.NotifyFilter = (NotifyFilters.FileName Or NotifyFilters.LastAccess Or NotifyFilters.LastWrite)
+            Watchers.Add(fsw)
+            AddHandler fsw.Created, AddressOf NewFile
+
+        Next
+
+    End Sub
+
+    Private Sub NewFile(sender As Object, e As FileSystemEventArgs)
         Dim PrintProcess As New Process
         Dim PrintProcessStartInfo As New ProcessStartInfo
 
@@ -17,31 +31,31 @@ Public Class Form1
             Do Until System.DateTime.Now >= FileSystem.FileDateTime(e.FullPath).AddSeconds(1)
                 System.Threading.Thread.Sleep(1000)
             Loop
-            MsgBox("Printing " & e.FullPath)
+            'MsgBox("Printing " & e.FullPath)
+
+#Disable Warning BC42025 ' Access of shared member, constant member, enum member or nested type through an instance
             PrintProcess.Start(PrintProcessStartInfo)
+#Enable Warning BC42025 ' Access of shared member, constant member, enum member or nested type through an instance
 
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
     End Sub
 
+
     Private Sub Start_Watching(sender As Object, e As EventArgs) Handles Me.Load, btnStart.Click
         Select Case My.Computer.FileSystem.DirectoryExists(My.Settings.WatchFolder)
             Case True
 
                 'todo: use a list and a loop to get this done, create the watchers in the method that geneerates the list.
-
-                XPSWatcher.Path = My.Settings.WatchFolder
-                XPSWatcher.EnableRaisingEvents = True
-
-                PDFWatcher.Path = My.Settings.WatchFolder
-                PDFWatcher.EnableRaisingEvents = True
-
-                JPGWatcher.Path = My.Settings.WatchFolder
-                JPGWatcher.EnableRaisingEvents = True
+                For Each watcher As FileSystemWatcher In Watchers
+                    watcher.Path = My.Settings.WatchFolder
+                    watcher.EnableRaisingEvents = True
+                Next
 
                 btnStart.Enabled = False
             Case False
+                btnStart.Enabled = True
                 Select Case sender.Equals(Me)
                     Case True
                         MsgBox(My.Resources.WelcomeText1 & vbNewLine & My.Resources.WelcomeText2, MsgBoxStyle.Information)
@@ -78,4 +92,6 @@ Public Class Form1
         RemoveHandler Me.FormClosing, AddressOf Window_Closing
         Me.Close()
     End Sub
+
+
 End Class
